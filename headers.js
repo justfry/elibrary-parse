4e67defa8c677026a5b9abd3e62eb3178ed4d097
justfry <dondimon777@gmail.com>
2019-01-28 22:27:17 +0300

    forgot to add params and headers

---
+++ headers.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "Accept": "	text/css,*/*;q=0.1",
+    "Connection": "keep-alive",
+    "Host": "elibrary.ru",
+    "Referer": "https://elibrary.ru/org_items.asp?orgsid=2179",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; ) Gecko/20100101 Firefox/64.0",
+}
